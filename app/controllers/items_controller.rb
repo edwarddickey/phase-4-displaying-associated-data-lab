422c19ccfceaee3d4b38745022c1a7854a99e106
Edward Dickey <edward.c.dickey@gmail.com>
2022-10-17 10:06:00 -0600

    commit tree 55245510de20ad3c9ad9cc756fe5147e90080be3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,11 @@
 class ItemsController < ApplicationController
+
+    def index
+        items = Item.all 
+        render json: items, include: :user
 end
+end
+    # def show 
+    #     items = Items.find_by(id: params[:id])
+    #     render json: items
+    # end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+    def index
+        users = Users.all 
+        render json: users
+end
+
+    def show 
+        users = User.find(params[:id])
+        render json: users, include: :items
+    end
 end
